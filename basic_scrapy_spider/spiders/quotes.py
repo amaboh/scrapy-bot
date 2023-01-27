91dc37112d2ae56387420d5e90146fdd02d72b26
amaboh <amabocanon@gmail.com>
2023-01-27 08:01:29 +0000

    initial commit

---
+++ basic_scrapy_spider/spiders/quotes.py
@@ -0,0 +1,11 @@
+import scrapy
+from basic_scrapy_spider.items import QuoteItem
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        pass
